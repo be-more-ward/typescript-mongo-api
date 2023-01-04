8fb7db5a0d314a770347991930485e67b5423a14
be-more-ward <119626790+be-more-ward@users.noreply.github.com>
2023-01-04 16:50:38 -0300

    feat security

--- app.ts
+++ app.ts
@@ -4,6 +4,13 @@ import "express-async-errors"
 import express from "express";
 const app = express()
 
+// import SECURITY
+import helmet from "helmet"
+import cors from "cors"
+import xss from "xss-clean"
+import rateLimiter from "express-rate-limit"
+
+
 //MIDDLEWARE IMPORTS
 import  {errorHandler} from "./middleware/error-handler";
 import {notFound} from "./middleware/not-found";
@@ -14,17 +21,19 @@ import authRouter from "./routes/auth.routes"
 import jobsRouter from "./routes/jobs.routes"
 
 
-
+app.use(rateLimiter({
+    windowMs: 15 * 60 * 1000, //15min
+    max: 100 //limit each IP to 100 request per windowMs
+}))
 app.use(express.json())
+app.use(helmet())
+app.use(cors())
+app.use(xss())
+
 
 // Routes
 app.use("/api/v1/auth", authRouter)
 app.use("/api/v1/jobs", authenticateUser, jobsRouter)
-
-app.post("/test", (req, res)=>{
-    const tt = req.body
-    res.status(200).json({msg: tt})
-})
 
 
 app.use(errorHandler)
